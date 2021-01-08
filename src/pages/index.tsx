feeb9ca9722f522ebc83f07223302ee6c37b0dd5
takeyu1013 <yuto1013@gmail.com>
2021-01-08 11:37:37 +0900

    mainコンポーネントのスタイルを追加しました

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,16 @@ const Home = () => {
           Read <Link href="/posts/first-post"><a>this page!</a></Link>
         </h1>
       </main>
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          dislplay: flex;
+          flexdirection: column;
+          justify-content: center;
+          align-items: center;
+        }
+      `}</style>
     </div>
   );
 };
