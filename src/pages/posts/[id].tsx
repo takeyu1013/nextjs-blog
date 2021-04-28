d93091e610e5935fa50c7689873003c957ff8f01
unknown <yuto1013@gmail.com>
2021-04-28 22:34:19 +0900

    外部データに依存するページのパス一覧を取得するプロパティを追加

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,4 +1,14 @@
+import { GetStaticPaths } from 'next';
 import { Layout } from '../../components/layout';
+import { getAllPostIds } from '../../lib/posts';
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false
+  };
+};
 
 const Post = () => {
   return <Layout>...</Layout>;
