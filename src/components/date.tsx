3e325f6a3f00698022b03dac38dcd54277516f2a
unknown <yuto1013@gmail.com>
2021-04-29 17:39:27 +0900

    コンポーネントをエクスポート

--- src/components/date.tsx
+++ src/components/date.tsx
@@ -4,3 +4,5 @@ const Date = ({ dateString }: { dateString: string }) => {
   const date = parseISO(dateString);
   return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
 };
+
+export default Date;
