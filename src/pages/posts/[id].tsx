78a8a22fa96c485da2e7b458d9bb0b66b5bb68ef
unknown <yuto1013@gmail.com>
2021-04-29 14:48:25 +0900

    静的生成プロパティに投稿データを追加

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,6 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { Layout } from '../../components/layout';
-import { getAllPostIds } from '../../lib/posts';
+import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
@@ -10,15 +10,22 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = getPostData(params?.id as string);
   return {
     props: {
-
+      postData
     }
   };
 };
 
-const Post = () => {
+const Post = ({ postData }: {
+  postData: {
+    title: string,
+    date: string,
+    content: string
+  }
+}) => {
   return <Layout>...</Layout>;
 };
 
