7458e7718f1c38ef6c0279b5ad49d9a13fedcd08
unknown <yuto1013@gmail.com>
2021-04-29 17:06:37 +0900

    マークダウンのコンテンツを取得する様に修正

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import remark from 'remark';
+import html from 'remark-html';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 
@@ -48,10 +50,13 @@ export const getAllPostIds = () => {
 
 export const getPostData = (id: string) => {
   const fullPath = path.join(postsDirectory, `${id}.md`);
-  const fileContents = fs.readFileSync(fullPath, 'utf-8');
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
   const matterResult = matter(fileContents);
+  const processedContent = remark().use(html).process(matterResult.content);
+  const contentHTML = processedContent.toString();
   return {
     id,
+    contentHTML,
     ...(matterResult.data as { date: string, title: string })
   };
 };
